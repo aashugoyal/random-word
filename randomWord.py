b91ef0750ce8b295003d126f27e79a5d36486a45
Gourav Sardana <gsardana001@gmail>
2018-01-19 00:35:04 +0530

    unUsedLine

--- randomWord.py
+++ randomWord.py
@@ -1,17 +1,8 @@
 import random
 a = 'a b c d e f g h i j k l'.split()
-random.shuffle(a)#here we have random shuffle form string
-y=a.pop() # value of a is store in y
-<<<<<<< HEAD
-print(y)#here 
-# hahahahahahahahaha
-=======
-print(y)#here
-
-
-
-'''
-doc string 
-
-'''
->>>>>>> branchEdit
+random.shuffle(a)
+#here we have random shuffle form string
+y=a.pop() 
+# value of a is store in y
+print(y)
+print(y)
